c8095781e4012183cd3ad98f630999ddd60b4d1f
Greg Mitchell <gam7491@gmail.com>
2022-12-17 12:22:29 -0800

    Refactor solution 6 to not eval on ns load

--- src/aoc/solution2022_6.clj
+++ src/aoc/solution2022_6.clj
@@ -3,32 +3,38 @@
             [clojure.string :as s]))
 
 
-(def input-file (io/resource "2022/6/input.txt"))
-(def input-line
+(def input-file "2022/6/input.txt")
+(defn input-line
+  []
   (-> input-file
+      io/resource
       slurp))
 
 (def start-of-packet-re
   #"(.)(?!\1)(.)(?!\1|\2)(.)(?!\1|\2|\3)(.)")
 
-(def packet-marker 
+(defn packet-marker 
+  [input-line]
   (first (re-find start-of-packet-re input-line)))
 
 (defn find-index-after
   [input marker]
   (+ (count marker) (s/index-of input marker)))
 
-(def packet-start
+(defn packet-start
+  [input-line packet-marker]
   (find-index-after input-line packet-marker))
 
 (def start-of-message-re
   #"(.)(?!\1)(.)(?!\1|\2)(.)(?!\1|\2|\3)(.)(?!\1|\2|\3|\4)(.)(?!\1|\2|\3|\4|\5)(.)(?!\1|\2|\3|\4|\5|\6)(.)(?!\1|\2|\3|\4|\5|\6|\7)(.)(?!\1|\2|\3|\4|\5|\6|\7|\8)(.)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9)(?<ten>.)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\k<ten>)(?<eleven>.)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\k<ten>|\k<eleven>)(?<twelve>.)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\k<ten>|\k<eleven>|\k<twelve>)(?<thirteen>.)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\k<ten>|\k<eleven>|\k<twelve>|\k<thirteen>)(.)")
 
-(def message-marker 
+(defn message-marker 
+  [input-line]
   (first (re-find start-of-message-re input-line)))
 
-(def message-start
-  (find-index-after input-line message-marker))
+(defn message-start
+  [input-line marker]
+  (find-index-after input-line marker))
 
 (defn match-and-find-message 
   [input]
